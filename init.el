c7e0d5b68ce9e7f9bc33dcdbd3a967cc10a38abe
ivoshet <ivan.v.shet@gmail.com>
2018-04-30 22:13:28 +0500

    add support for any js files

--- init.el
+++ init.el
@@ -153,6 +153,7 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
 
 (setq web-mode-enable-current-element-highlight t)
 
